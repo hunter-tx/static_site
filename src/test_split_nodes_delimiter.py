79f10f93d325e6c96d00b825b6c22862d261417b
hunter-tx <hughesh97@outlook.com>
2025-01-28 21:06:38 -0600

    Created split node functions for images and links to convert TextTypt.TEXT TextNodes into a list of TextNodes to include all necessary TextTypes

--- src/test_split_nodes_delimiter.py
+++ src/test_split_nodes_delimiter.py
@@ -1,8 +1,8 @@
 import unittest
 
-from split_delimiter import *
+from inline_markdown import *
 
-class TestSplitNodesDelimiter(unittest.TestCase):
+class TestSplitNodes(unittest.TestCase):
     def test_code(self):
         node = TextNode("This is text with a `code block` word", TextType.TEXT)
         new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
@@ -39,6 +39,32 @@ class TestSplitNodesDelimiter(unittest.TestCase):
         node = TextNode("This is all code", TextType.CODE)
         new_nodes = split_nodes_delimiter([node], '`', TextType.CODE)
         expected = [TextNode("This is all code", TextType.CODE)]
+        self.assertEqual(new_nodes, expected)
+
+    def test_link(self):
+        node = TextNode(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        expected = [
+            TextNode("This is text with a link ", TextType.TEXT),
+            TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+            TextNode(" and ", TextType.TEXT),
+            TextNode("to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev")
+        ]
+        self.assertEqual(new_nodes, expected)
+
+    def test_image(self):
+        node = TextNode(
+            "This is a node with ![an image](www.image.com) embedded", TextType.TEXT
+        )
+        new_nodes = split_nodes_image([node])
+        expected = [
+            TextNode("This is a node with ", TextType.TEXT),
+            TextNode("an image", TextType.IMAGE, "www.image.com"),
+            TextNode(" embedded", TextType.TEXT)
+        ]
         self.assertEqual(new_nodes, expected)
 
 
