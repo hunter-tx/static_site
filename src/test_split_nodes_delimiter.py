9a5d403e11de6e9fca0bd15ab04b6ac4e6daff2c
hunter-tx <hughesh97@outlook.com>
2025-02-05 14:16:17 -0600

    Created function to split blocks of markdown text into lists of strings of blocks

--- src/test_split_nodes_delimiter.py
+++ src/test_split_nodes_delimiter.py
@@ -67,23 +67,58 @@ class TestSplitNodes(unittest.TestCase):
     #     ]
     #     self.assertEqual(new_nodes, expected)
 
-    def test_text_to_textnode(self):
-        text = ("This is **text** with an *italic* word and a `code block` and an ![obi wan image]"
-                "(https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)")
-        test = text_to_textnodes(text)
-        expected = [
-            TextNode("This is ", TextType.TEXT),
-            TextNode("text", TextType.BOLD),
-            TextNode(" with an ", TextType.TEXT),
-            TextNode("italic", TextType.ITALIC),
-            TextNode(" word and a ", TextType.TEXT),
-            TextNode("code block", TextType.CODE),
-            TextNode(" and an ", TextType.TEXT),
-            TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
-            TextNode(" and a ", TextType.TEXT),
-            TextNode("link", TextType.LINK, "https://boot.dev"),
-        ]
+    # def test_text_to_textnode(self):
+    #     text = ("This is **text** with an *italic* word and a `code block` and an ![obi wan image]"
+    #             "(https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)")
+    #     test = text_to_textnodes(text)
+    #     expected = [
+    #         TextNode("This is ", TextType.TEXT),
+    #         TextNode("text", TextType.BOLD),
+    #         TextNode(" with an ", TextType.TEXT),
+    #         TextNode("italic", TextType.ITALIC),
+    #         TextNode(" word and a ", TextType.TEXT),
+    #         TextNode("code block", TextType.CODE),
+    #         TextNode(" and an ", TextType.TEXT),
+    #         TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+    #         TextNode(" and a ", TextType.TEXT),
+    #         TextNode("link", TextType.LINK, "https://boot.dev"),
+    #     ]
+    #     self.assertEqual(test, expected)
+
+    def test_split_block(self):
+        markdown = ("# This is a heading\n"
+                 "\nThis is a paragraph of text. It has some **bold** and *italic* words inside of it.\n"
+                 "\n* This is the first list item in a list block\n* This is a list item"
+                 "\n* This is another list item")
+        test = markdown_to_blocks(markdown)
+        expected = ["# This is a heading",
+                    "This is a paragraph of text. It has some **bold** and *italic* words inside of it.",
+                    "* This is the first list item in a list block\n* This is a list item\n* This is another list item"
+                    ]
         self.assertEqual(test, expected)
+
+    def test_extra_lines(self):
+        markdown = ("# This is the heading\n\n"
+                    "This is the first line of text\n\n\n\n\n"
+                    "There are too many new lines here\n\n"
+                    "This one is good\n\n\n"
+                    "This one is not")
+        test = markdown_to_blocks(markdown)
+        expected = ["# This is the heading", "This is the first line of text", "There are too many new lines here",
+                    "This one is good", "This one is not"]
+        self.assertEqual(test, expected)
+
+    def test_strip_whitespace(self):
+        markdown = ("     # This is the heading     \n\n"
+                    "  This is the first line of text \n\n\n\n\n"
+                    " There are too many new lines here    \n\n"
+                    "   This one is good  \n\n\n"
+                    "       This one is not")
+        test = markdown_to_blocks(markdown)
+        expected = ["# This is the heading", "This is the first line of text", "There are too many new lines here",
+                    "This one is good", "This one is not"]
+        self.assertEqual(test, expected)
+
 
 
 
